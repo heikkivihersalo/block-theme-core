aefd5253adbbcc9c0d5971ac29aa6b7781198223
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-20 19:10:11 +0200

    add editor styles method

--- src/Support/Assets/Style.php
+++ src/Support/Assets/Style.php
@@ -21,10 +21,11 @@ class Style extends Asset {
 	 * @param string $asset The asset file path
 	 * @param int    $priority The priority of the enqueued asset
 	 * @param bool   $admin Whether the asset is for admin or not
+	 * @param bool   $editor Whether the asset is for `add_editor_style` function or not
 	 * @return self
 	 */
-	public static function create( string $handle, string $src, string $asset, int $priority = 10, bool $admin = false ): self {
-		return new self( $handle, $src, '', $asset, $priority, $admin );
+	public static function create( string $handle, string $src, string $asset, int $priority = 10, bool $admin = false, bool $editor = false ): self {
+		return new self( $handle, $src, '', $asset, $priority, $admin, $editor );
 	}
 
 	/**
@@ -57,6 +58,11 @@ class Style extends Asset {
 	 */
 	public function register(): void {
 		$action = $this->is_admin() ? 'admin_enqueue_scripts' : 'wp_enqueue_scripts';
+
+		if ( $this->is_editor() ) {
+			$this->loader->add_action( 'admin_init', $this, 'add_to_editor_styles' );
+		}
+
 		$this->app->make( HooksLoader::class )->add_action( $action, $this, 'enqueue', $this->get_priority() );
 	}
 }
