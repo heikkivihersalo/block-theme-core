224a4ad1a15795c466182582c31856111d98d000
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-20 19:09:30 +0200

    style formatting fixes

--- src/Admin/CustomizerProvider.php
+++ src/Admin/CustomizerProvider.php
@@ -4,10 +4,15 @@ namespace Vihersalo\BlockThemeCore\Admin;
 
 use Vihersalo\BlockThemeCore\Application;
 use Vihersalo\BlockThemeCore\Application\HooksLoader;
-
 use Vihersalo\BlockThemeCore\Support\Utils\Common as Utils;
 use Vihersalo\BlockThemeCore\Support\ServiceProvider;
 
+/**
+ *
+ * @since      2.0.0
+ * @package    Vihersalo\BlockThemeCore\Admin
+ * @author     Heikki Vihersalo <heikki@vihersalo.fi>
+ */
 class CustomizerProvider extends ServiceProvider {
 	/**
 	 * Register the navigation provider
--- src/Admin/Duplicate/DuplicateProvider.php
+++ src/Admin/Duplicate/DuplicateProvider.php
@@ -1,37 +1,43 @@
-<?php 
+<?php
 
 namespace Vihersalo\BlockThemeCore\Admin\Duplicate;
 
 use Vihersalo\BlockThemeCore\Application;
 use Vihersalo\BlockThemeCore\Application\HooksLoader;
-use Vihersalo\BlockThemeCore\Admin\Duplicate\Helpers;
+use Vihersalo\BlockThemeCore\Admin\Duplicate\Utils;
 use Vihersalo\BlockThemeCore\Support\ServiceProvider;
 
+/**
+ *
+ * @since      2.0.0
+ * @package    Vihersalo\BlockThemeCore\Admin\Duplicate
+ * @author     Heikki Vihersalo <heikki@vihersalo.fi>
+ */
 class DuplicateProvider extends ServiceProvider {
-    /**
-     * Register the navigation provider
-     */
-    public function register() {
-        $this->enable_duplicate_posts();
-    }
+	/**
+	 * Register the navigation provider
+	 */
+	public function register() {
+		$this->enable_duplicate_posts( $this->app->make( HooksLoader::class ) );
+	}
 
-    /**
+	/**
 	 * Enable customizer
 	 *
 	 * @since    2.0.0
 	 * @access   private
 	 * @return   void
 	 */
-	private function enable_duplicate_posts() {
-        $this->app->make(HooksLoader::class)->add_action( 'admin_action_create_duplicate_post_as_draft', Helpers::class, 'create_duplicate_post_as_draft' );
-		$this->app->make(HooksLoader::class)->add_filter( 'post_row_actions', Helpers::class, 'add_duplicate_post_link_to_admin', 10, 2 );
-		$this->app->make(HooksLoader::class)->add_filter( 'page_row_actions', Helpers::class, 'add_duplicate_post_link_to_admin', 10, 2 );
-		$this->app->make(HooksLoader::class)->add_action( 'admin_notices', Helpers::class, 'show_duplicate_admin_notice' );
+	private function enable_duplicate_posts( HooksLoader $loader ) {
+		$loader->add_action( 'admin_action_create_duplicate_post_as_draft', Utils::class, 'create_duplicate_post_as_draft' );
+		$loader->add_filter( 'post_row_actions', Utils::class, 'add_duplicate_post_link_to_admin', 10, 2 );
+		$loader->add_filter( 'page_row_actions', Utils::class, 'add_duplicate_post_link_to_admin', 10, 2 );
+		$loader->add_action( 'admin_notices', Utils::class, 'show_duplicate_admin_notice' );
 	}
 
-    /**
-     * Boot the navigation provider
-     */
-    public function boot() {
-    }
+	/**
+	 * Boot the navigation provider
+	 */
+	public function boot() {
+	}
 }
