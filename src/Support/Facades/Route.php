47df36a0b2bd480ec3b5cd88d163aae37f40e783
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-26 20:01:52 +0200

    add type hinting

--- src/Support/Facades/Route.php
+++ src/Support/Facades/Route.php
@@ -1,9 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Vihersalo\Core\Support\Facades;
 
-use Vihersalo\Core\Api\Router;
+use Vihersalo\Core\Api;
 
+/**
+ * @method static \Vihersalo\Core\Api get(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api post(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api put(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api patch(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api delete(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api options(string $uri, array|string|callable|null $action = null)
+ * @method static \Vihersalo\Core\Api any(string $uri, array|string|callable|null $action = null)
+ *
+ * @see Api
+ */
 class Route extends Facade {
     /**
      * Get the registered name of the component.
