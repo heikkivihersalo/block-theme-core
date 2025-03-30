9048184ec02b7c2580cd58e7de3b8f82a716df74
Heikki Vihersalo <heikki@vihersalo.fi>
2025-03-30 19:45:52 +0300

    fix method calls

--- src/Support/Facades/Settings.php
+++ src/Support/Facades/Settings.php
@@ -9,9 +9,9 @@ use Vihersalo\Core\Admin\Settings\SettingsMenu;
 // phpcs:disable
 /**
  * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu create(string $slug, string $pageTitle, string $menuTitle, string $path, string $capability, string $icon, int $position)
- * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withSubmenu(?callable $callback = null): self
- * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withAssets(?callable $callback = null): self
- * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withLocalize(string $handle, string $objectName, array $l10n): self
+ * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withSubmenu(?callable $callback = null)
+ * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withAssets(?callable $callback = null)
+ * @method static \Vihersalo\Core\Admin\Settings\SettingsMenu withLocalize(string $handle, string $objectName, array $l10n)
  * @see SettingsMenu
  */
 class Settings extends Facade {
