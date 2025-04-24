939e58cd0267d1474b10180b8702b5549c326943
Heikki Vihersalo <heikki@vihersalo.fi>
2025-04-24 19:06:42 +0300

    add formatting functions

--- src/Support/Utils/Formatting.php
+++ src/Support/Utils/Formatting.php
@@ -24,6 +24,33 @@ final class Formatting {
     }
 
     /**
+     * Get URI for file
+     * @param  string $path Path to append to base path
+     * @return string
+     */
+    public static function trimFilePath(string $path = ''): string {
+        return rtrim($this->base_uri, '/\\') . '/' . $path;
+    }
+
+    /**
+     * Write code on Method
+     * @param  string $file String to convert
+     * @return string
+     */
+    public static function camelcaseToKebabcase(string $file) {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '-$0', $file));
+    }
+
+    /**
+     * Remove file extension from string
+     * @param  string $file String to convert
+     * @return string
+     */
+    public static function removeFileExtension(string $file) {
+        return str_replace('.php', '', $file);
+    }
+
+    /**
      * Format phone number to Finnish format
      * @param mixed $num Phone number
      * @return string
