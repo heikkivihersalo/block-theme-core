5618c17a165c9474b455dc217e2d97d279b0d25b
Heikki Vihersalo <heikki@vihersalo.fi>
2025-04-27 15:29:26 +0300

    use data attributes instead of classes

--- src/PostTypes/Assets/post-types.js
+++ src/PostTypes/Assets/post-types.js
@@ -1,40 +1,44 @@
 const imageUploaderElements = {
-    uploadImage: document.querySelector('.app-post-type-image-uploader__button--choose'),
-    imageInput: document.querySelector('.app-post-type-image-uploader__input'),
-    removeImage: document.querySelector('.app-post-type-image-uploader__button--remove'),
-    imagePreview: document.querySelector('.app-post-type-image-uploader__preview')
+	uploadImage: document.querySelector(
+		'.app-post-type-image-uploader__button--choose'
+	),
+	imageInput: document.querySelector('.app-post-type-image-uploader__input'),
+	removeImage: document.querySelector(
+		'.app-post-type-image-uploader__button--remove'
+	),
+	imagePreview: document.querySelector(
+		'.app-post-type-image-uploader__preview'
+	),
 };
-
-const isHiddenSelector = 'hide-app-post-type-image-uploader';
 
 /**
  * Handle the image uploader
  */
 document.addEventListener('DOMContentLoaded', () => {
-    /**
-     * Check if elements exist on the page (in other words, if the page has the image uploader)
-     */
-    const elementsExist = Object.values(imageUploaderElements).every(
-        (element) => element !== null
-    );
+	/**
+	 * Check if elements exist on the page (in other words, if the page has the image uploader)
+	 */
+	const elementsExist = Object.values(imageUploaderElements).every(
+		(element) => element !== null
+	);
 
-    if (!elementsExist) {
-        return;
-    }
+	if (!elementsExist) {
+		return;
+	}
 
-    /**
-     * Handle Select Image
-     */
-    imageUploaderElements.uploadImage.addEventListener('click', (e) => {
-        handleSelectImage(e);
-    });
+	/**
+	 * Handle Select Image
+	 */
+	imageUploaderElements.uploadImage.addEventListener('click', (e) => {
+		handleSelectImage(e);
+	});
 
-    /**
-     * Handle Remove Image
-     */
-    imageUploaderElements.removeImage.addEventListener('click', (e) => {
-        handleRemoveImage(e);
-    });
+	/**
+	 * Handle Remove Image
+	 */
+	imageUploaderElements.removeImage.addEventListener('click', (e) => {
+		handleRemoveImage(e);
+	});
 });
 
 /**
@@ -43,51 +47,53 @@ document.addEventListener('DOMContentLoaded', () => {
  * @return {void}
  */
 function handleSelectImage(e) {
-    e.preventDefault();
-    try {
-        let frame;
+	e.preventDefault();
+	try {
+		let frame;
 
-        if (frame) {
-            frame.open();
-            return;
-        }
+		if (frame) {
+			frame.open();
+			return;
+		}
 
-        frame = wp.media({
-            title: 'Select or Upload Media',
-            button: {
-                text: 'Use this media',
-            },
-            multiple: false,
-        });
+		frame = wp.media({
+			title: 'Select or Upload Media',
+			button: {
+				text: 'Use this media',
+			},
+			multiple: false,
+		});
 
-        frame.on('select', () => {
-            const attachment = frame.state().get('selection').first();
+		frame.on('select', () => {
+			const attachment = frame.state().get('selection').first();
 
-            /* Place ID to the input field */
-            imageUploaderElements.imageInput.value = attachment.id;
+			/* Place ID to the input field */
+			imageUploaderElements.imageInput.value = attachment.id;
 
-            /* Show image preview */
-            imageUploaderElements.imagePreview.setAttribute(
-                'src',
-                attachment.attributes.url
-            );
-            imageUploaderElements.imagePreview.classList.remove(
-                isHiddenSelector
-            );
+			/* Show image preview */
+			imageUploaderElements.imagePreview.setAttribute(
+				'src',
+				attachment.attributes.url
+			);
+			imageUploaderElements.imagePreview.setAttribute(
+				'data-hidden',
+				false
+			);
 
-            /* Show "Remove" button */
-            imageUploaderElements.removeImage.classList.remove(
-                isHiddenSelector
-            );
+			/* Show "Remove" button */
+			imageUploaderElements.removeImage.setAttribute(
+				'data-hidden',
+				false
+			);
 
-            /* Hide "Choose" button */
-            imageUploaderElements.uploadImage.classList.add(isHiddenSelector);
-        });
+			/* Hide "Choose" button */
+			imageUploaderElements.uploadImage.setAttribute('data-hidden', true);
+		});
 
-        frame.open();
-    } catch (error) {
-        console.error(error);
-    }
+		frame.open();
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 /**
@@ -96,18 +102,18 @@ function handleSelectImage(e) {
  * @return {void}
  */
 function handleRemoveImage(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    /* Remove ID from the input field */
-    imageUploaderElements.imageInput.value = '';
+	/* Remove ID from the input field */
+	imageUploaderElements.imageInput.value = '';
 
-    /* Hide the preview image */
-    imageUploaderElements.imagePreview.setAttribute('src', '');
-    imageUploaderElements.imagePreview.classList.add(isHiddenSelector);
+	/* Hide the preview image */
+	imageUploaderElements.imagePreview.setAttribute('src', '');
+	imageUploaderElements.imagePreview.setAttribute('data-hidden', true);
 
-    /* Show "Choose" button */
-    imageUploaderElements.uploadImage.classList.remove(isHiddenSelector);
+	/* Show "Choose" button */
+	imageUploaderElements.uploadImage.setAttribute('data-hidden', false);
 
-    /* Hide "Remove" button */
-    imageUploaderElements.removeImage.classList.add(isHiddenSelector);
+	/* Hide "Remove" button */
+	imageUploaderElements.removeImage.setAttribute('data-hidden', true);
 }
